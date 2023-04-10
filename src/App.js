c9993636db38fdaff7adbcadfeeddb98a543994a
Smriti Maharjan <smriti.maharjan@swifttech.com>
2023-04-10 03:28:35 -0700

    Small Changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { NavBar ,Footer} from './components';
 function App() {
   return (
     <div className="App">
-      Hello World!!
+      <NavBar/>
+      <Footer/>
     </div>
   );
 }
