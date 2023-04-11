fc3fa19e2418ea3d5cf95c0a8240ace5f5011958
Smriti-Mhrz <smriti.maharjan@swifttech.com>
2023-04-10 21:50:22 -0700

    navbar changes made

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,17 @@ import logo from '../assets/images/logo.webp'
 const navList= ['Home', 'Products', 'Services', 'Portfolio','Team','Blog','Contact']
 export default function NavBar() {
   return (
-   
+    <header>
+      <div className='logo'>
+        <img src={logo} alt="logo"/>
+      </div>
+      <div className='menu-links'>
+        <ul>
+          {navList.map(Item)=>{
+            return '<li><NavLink></NavLink></li>' 
+          }}
+        </ul>
+      </div>
+    </header>
   )
 }
