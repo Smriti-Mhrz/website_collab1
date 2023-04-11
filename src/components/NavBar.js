47c08fa91f159946a21284f8a9ab45c7fcf427dd
Smriti Maharjan <smriti.maharjan@swifttech.com>
2023-04-10 21:32:19 -0700

    image added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import logo from '../assets/images/logo.webp'
 
+const navList= ['Home', 'Products', 'Services', ]
 export default function NavBar() {
   return (
-    <div>
-      NavBar
-    </div>
+    <header>
+      <div className='logo'>
+        <img src={logo} alt="logo"/>
+      </div>
+      <div className='menu-links'>
+        <ul>
+          
+        </ul>
+      </div>
+    </header>
   )
 }
