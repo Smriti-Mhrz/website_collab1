662ca777e50cd640f24d1c2b86e81bc0687c1cde
Smriti Maharjan <smriti.maharjan@swifttech.com>
2023-04-10 21:45:13 -0700

    Change in NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,9 @@
 import React from 'react'
 import logo from '../assets/images/logo.webp'
 
-const navList= ['Home', 'Products', 'Services', ]
+const navList= ['Home', 'Products', 'Services', 'Portfolio','Team','Blog','Contact']
 export default function NavBar() {
   return (
-    <header>
-      <div className='logo'>
-        <img src={logo} alt="logo"/>
-      </div>
-      <div className='menu-links'>
-        <ul>
-          
-        </ul>
-      </div>
-    </header>
+   
   )
 }
