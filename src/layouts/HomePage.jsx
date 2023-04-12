679763a169f193be6dc8bad8600eee7fd0e4eaf7
Smriti Maharjan <Smritimaharjan99@gmail.com>
2023-04-12 09:05:04 +0545

    naavbar css changed

--- src/layouts/HomePage.jsx
+++ src/layouts/HomePage.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { Footer, NavBar } from '../components'
-import { Outlet } from 'react-router-dom'
 import Home from '../pages/Home'
 
 export default function HomePage() {
   return (
     <>
         <NavBar/>
-        <Home/>
+          <Home/>
         <Footer/>
     </>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import '../assets/style/page/Home.scss'
 import banner from '../assets/images/main_banner.webp'
-import BsArrowRight from 'react-icons/bs'
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa'
 
 export default function Home() {
   return (
     <div className='container__home'>
-      <div className='wrapper__home'>
         <div className='image'>
           <img src={banner} alt="main_banner" />    
           {/* <div className="banner__circle">
@@ -15,10 +14,16 @@ export default function Home() {
         </div>
         <div className='banner__desc'>
           <h1><span>Sofa</span> and <span>Armchairs</span></h1>
-          <p>One day however a small line of blind text by the name of Lorem Ipsum </p>
+          <p>One day however a small line of blind text by the name of Lorem Ipsum<br/> decided to leave for the far World of Grammar.<br/> </p>
           <button>Learn More</button>
+          <div className='social-media'>
+              <ul>
+                <li><FaFacebook/></li>
+                <li><FaTwitter/></li>
+                <li><FaInstagram/></li>
+              </ul>
+          </div>
         </div>
-      </div>
     </div>
   )
 }
